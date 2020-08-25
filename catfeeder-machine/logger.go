ea70deda56deb5e3170510d10798dddb76ef3534
loerac <chatoloera18@gmail.com>
2020-08-24 21:41:36 -0500

    Updated logger to roll over on existing log files
    On initialization, it'll check if there exists an existing log file. If so, prepend filename with a timestamp, and then make a new file.
    On creation of a new logger, give them the File pointer to the file so they can write to the file with their prefix

---
+++ catfeeder-machine/logger.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "os"
+    "strings"
+    "sync"
+    "time"
+)
+
+type Logger struct {
+    Lock        sync.Mutex
+    Fpath       *os.File
+    Prefix      string
+}
+
+const LogFilename string = "/tmp/cat-feeder.log"
+
+var Fpath *os.File
+
+/**
+ * @brief:  Checks to see if the log file is present from a previous
+ *          execution. If true, roll over log with timestamp. Then
+ *          open a new log file.
+ *
+ * @return: nil on success, else error
+ **/
+func LoggerInit() (err error) {
+    finfo, err := os.Stat(LogFilename)
+    if err == nil {
+        path := strings.Split(LogFilename, finfo.Name())[0]
+        err = os.Rename(
+            LogFilename,
+            path + time.Now().Format("20060102T150405") + "-" + finfo.Name(),
+        )
+        if err != nil {
+            return err
+        }
+    }
+
+    Fpath, err = os.Create(LogFilename)
+    return err
+}
+
+/**
+ * @brief:  Create a Logger with prefix and filepath
+ *
+ * @arg:    prefix - String to prepend to the log message
+ *
+ * @return: New logger, nil if error
+ **/
+func NewLogger(prefix string) *Logger {
+    return &Logger{
+        Prefix: prefix,
+        Fpath: Fpath,
+    }
+}
+
+/**
+ * @brief:  Write log message to the log file. Log message contains:
+ *          - Timestamp
+ *          - Prefix
+ *          - Message
+ *
+ * @arg:    msg - Message to write to log file
+ *
+ * @return: int - How many bytes were written
+ *          error - Errors while writing
+ **/
+func (l *Logger) Println(msg string) (int, error) {
+    l.Lock.Lock()
+    defer l.Lock.Unlock()
+    output := []byte(time.Now().Format("2006/01/02 15:04:05") + " " + l.Prefix + " - " + msg + "\n")
+    return l.Fpath.Write(output)
+}
