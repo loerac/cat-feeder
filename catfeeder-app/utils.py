6dfcc8283d8eee8b9a061a1f999d9dd55d0f43c3
loerac <chatoloera@gmail.com>
2020-08-27 09:02:50 -0500

    Added comments and changed logs/labels
    Logging: Instead of having all the logs as 'error', changed some to be 'info'
    Label: Instead of printing some error messages to the screen, display them on the app

--- catfeeder-app/utils.py
+++ catfeeder-app/utils.py
@@ -1,7 +1,14 @@
 import logging
 
+###
+# @brief:   Create new logging object with the identity of `name`
+#
+# @arg:     name - The name to give the identity of the log
+#
+# @return:  Logger object
+###
 def Applogger(name):
-    logging.basicConfig(level=logging.WARNING,
+    logging.basicConfig(level=logging.INFO,
                         filename='/tmp/cat-feeder.log',
                         format='%(asctime)s %(name)s - %(message)s',
                         datefmt='%Y/%m/%d %H:%M:%S'
@@ -11,11 +18,25 @@ def Applogger(name):
 
     return logger
 
+###
+# @brief:   Turn the feeding time input to the time formate
+#
+# @arg:     feeding_time - Array holding the hour and minute
+#
+# @return:  String formated time
+###
 def PrettyTime(feeding_times):
     hour = feeding_times[0]
     minute = feeding_times[1]
 
     return str(hour) + ":" + (str(minute) if int(minute) > 9 else "0" + str(minute))
 
-def StrError(err):
+###
+# @brief:   Parse the OS error to return the error message
+#
+# @arg:     err - OSError from try-except
+#
+# @return:  String formated error message
+###
+def StrOSError(err):
     return str(err.args[0].reason)[str(err.args[0].reason).find(":") + 2:]
